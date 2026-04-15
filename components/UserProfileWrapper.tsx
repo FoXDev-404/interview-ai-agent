fc3d9cf453ac4789969f759e8147a4459ba20625
Rajpal Nishad <rajpal010304@gmail.com>
2026-04-15 22:45:04 +0530

    chore: remove unused components and assets to clean up the codebase

--- components/UserProfileWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { getCurrentUser } from '@/lib/auth';
-import UserProfile from './UserProfile';
-
-const UserProfileWrapper = async () => {
-  const user = await getCurrentUser();
-  return <UserProfile user={user} />;
-};
-
-export default UserProfileWrapper;
